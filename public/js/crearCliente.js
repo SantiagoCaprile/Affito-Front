e0ada741b0d682246510aa1025eb9704afa5349e
Santiago Caprile <santiagocaprile7@gmail.com>
2022-09-07 12:22:00 -0300

    Validaciones hechas para el front del formulario de creacion de cliente

--- public/js/crearCliente.js
+++ public/js/crearCliente.js
@@ -17,6 +17,193 @@ window.onload = function() {
     const dpto = document.getElementById('dpto')
     const localidad = document.getElementById('localidad')
 
+    let validarNombre = () => {
+        let valido = true;
+        let error = document.getElementById("nombre-error");
+        error.classList.add("oculto");
+        if(nombre.value.length > 60){
+            valido = false;
+            error.innerHTML = "El nombre no puede tener más de 60 caracteres";
+            error.classList.remove("oculto");
+        } else if (nombre.value.length == 0) {
+            valido = false;
+            error.innerHTML = "El nombre no puede estar vacío";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarCuit = () => {
+        let valido = true;
+        let error = document.getElementById("cuit-error");
+        error.classList.add("oculto");
+        if(cuit.value.length > 11){
+            valido = false;
+            error.innerHTML = "El CUIT/CUIL no puede tener más de 11 caracteres";
+            error.classList.remove("oculto");
+        } else if (cuit.value.length == 0) {
+            valido = false;
+            error.innerHTML = "El CUIT/CUIL no puede estar vacío";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarIVA = () => {
+        let valido = true;
+        let error = document.getElementById("iva-error");
+        error.classList.add("oculto");
+        if(iva.value == ""){
+            valido = false;
+            error.innerHTML = "Debe seleccionar una opción";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarMail = () => {
+        let valido = true;
+        let error = document.getElementById("mail-error");
+        error.classList.add("oculto");
+        if(mail.value.length != 0){
+            if(mail.value.length > 50){
+                valido = false;
+                error.innerHTML = "El mail no puede tener más de 60 caracteres";
+                error.classList.remove("oculto");
+            } else if (!mail.value.match(/^\S+@[a-z]+\.[a-z]/)) {
+                valido = false;
+                error.innerHTML = "El mail no tiene un formato válido";
+                error.classList.remove("oculto");
+            }
+        }
+        return valido;
+    }
+
+    let validarCelTel = () => {
+        let valido = true;
+        let error = document.getElementById("cel-error");
+        let errorTel = document.getElementById("tel-error");
+        error.classList.add("oculto");
+        errorTel.classList.add("oculto");
+        if(cel.value.length == 0 && tel.value.length == 0){
+            valido = false;
+            error.innerHTML = "Debe ingresar al menos un número de teléfono";
+            error.classList.remove("oculto");
+        } else {
+            if(cel.value.length != 0){
+                if (cel.value.length > 20) {
+                    valido = false;
+                    error.innerHTML = "El celular no puede tener más de 20 caracteres";
+                    error.classList.remove("oculto");
+                } else if (!cel.value.match(/^[0-9]+$/)) {
+                    valido = false;
+                    error.innerHTML = "El celular no puede tener letras";
+                    error.classList.remove("oculto");
+                }
+            }
+            if(tel.value.length != 0){
+                if (tel.value.length > 20) {
+                    valido = false;
+                    errorTel.innerHTML = "El teléfono no puede tener más de 20 caracteres";
+                    errorTel.classList.remove("oculto");
+                } else if (!tel.value.match(/^[0-9]+$/)) {
+                    valido = false;
+                    errorTel.innerHTML = "El teléfono no tiene un formato válido";
+                    errorTel.classList.remove("oculto");
+                }
+            }
+        }
+        return valido;
+    }
+
+    let validarLocalidad = () => {
+        let valido = true;
+        let error = document.getElementById("localidad-error");
+        error.classList.add("oculto");
+        if(localidad.value == ""){
+            valido = false;
+            error.innerHTML = "Debe seleccionar una opción";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarCalle = () => {
+        let valido = true;
+        let error = document.getElementById("calle-error");
+        error.classList.add("oculto");
+        if(calle.value.length > 50){
+            valido = false;
+            error.innerHTML = "La calle no puede tener más de 50 caracteres";
+            error.classList.remove("oculto");
+        } else if (calle.value.length == 0) {
+            valido = false;
+            error.innerHTML = "La calle no puede estar vacía";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarAltura = () => {
+        let valido = true;
+        let error = document.getElementById("altura-error");
+        error.classList.add("oculto");
+        if(altura.value.length > 6){
+            valido = false;
+            error.innerHTML = "La altura no puede tener más de 6 caracteres";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    let validarPiso = () => {
+        let valido = true;
+        let error = document.getElementById("piso-error");
+        error.classList.add("oculto");
+        if(piso.value.length > 3){
+            valido = false;
+            error.innerHTML = "El piso no puede tener más de 3 caracteres";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    function validarDpto(){
+        let valido = true;
+        let error = document.getElementById("dpto-error");
+        error.classList.add("oculto");
+        if(dpto.value.length > 3){
+            valido = false;
+            error.innerHTML = "El dpto no puede tener más de 3 caracteres";
+            error.classList.remove("oculto");
+        }
+        return valido;
+    }
+
+    function camposValidos() {
+        let todosValidos = true;
+        //nombre - max 60 - requerido
+        todosValidos = validarNombre() && todosValidos;
+        //cuit - max 11 - solo numerico - requerido
+        todosValidos = validarCuit() && todosValidos;
+        //iva - requerido
+        todosValidos = validarIVA() && todosValidos;
+        //mail - max 50 - regex
+        todosValidos = validarMail() && todosValidos;
+        //cel y tel - max 20 - solo numerico - requerido uno al menos
+        todosValidos = validarCelTel() && todosValidos;
+        //localidad - requerido
+        todosValidos = validarLocalidad() && todosValidos;
+        //calle - max 50 - requerido
+        todosValidos = validarCalle() && todosValidos;
+        //altura max 6
+        todosValidos = validarAltura() && todosValidos;
+        //piso y dpto - max 3
+        todosValidos = validarPiso() && todosValidos;
+        todosValidos = validarDpto() && todosValidos;
+        return todosValidos;
+    }
+
     //si recibe un cuit por el query string
     let ncuit = window.location.search.split('=')[1];
     let busqueda = async() => {
@@ -49,6 +236,35 @@ window.onload = function() {
             btn.innerHTML = "Editar cliente";
             btn.onclick = (e) => {
                 e.preventDefault()
+                if(camposValidos()) {
+                    const cliente = new Cliente(
+                        cuit.value,
+                        nombre.value,
+                        cel.value,
+                        tel.value,
+                        mail.value.toLowerCase(),
+                        iva.value,
+                        calle.value,
+                        altura.value,
+                        piso.value,
+                        dpto.value,
+                        localidad.value
+                    )
+                    cliente.editarCliente();
+                    alert("Editado correctamente")
+                    window.location.href = "/public/clientes.html"
+                }
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            document.getElementsByTagName("h1")[0].innerHTML = "Error";
+            document.getElementsByClassName("contenido")[0].innerHTML = "No se encontro el cliente";
+        });
+    } else { //si no recibe un cuit por el query string
+        btn.onclick = (e) => {
+            e.preventDefault()
+            if(camposValidos()) {
                 const cliente = new Cliente(
                     cuit.value,
                     nombre.value,
@@ -62,35 +278,10 @@ window.onload = function() {
                     dpto.value,
                     localidad.value
                 )
-                cliente.editarCliente();
-                alert("Editado correctamente")
+                cliente.guardarCliente()
+                alert("Cargado correctamente")
                 window.location.href = "/public/clientes.html"
             }
-        })
-        .catch(err => {
-            console.log(err);
-            document.getElementsByTagName("h1")[0].innerHTML = "Error";
-            document.getElementsByClassName("contenido")[0].innerHTML = "No se encontro el cliente";
-        });
-    } else { //si no recibe un cuit por el query string
-        btn.onclick = (e) => {
-            e.preventDefault()
-            const cliente = new Cliente(
-                cuit.value,
-                nombre.value,
-                cel.value,
-                tel.value,
-                mail.value,
-                iva.value,
-                calle.value,
-                altura.value,
-                piso.value,
-                dpto.value,
-                localidad.value
-            )
-            cliente.guardarCliente()
-            alert("Cargado correctamente")
-            window.location.href = "/public/clientes.html"
         };
     }
 };
