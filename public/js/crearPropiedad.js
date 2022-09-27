c3a10ac626f7933b23234625164a1278b7103dba
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2022-09-27 17:24:25 -0300

    Creacion de Modal y su funcionalidad correcta

--- public/js/crearPropiedad.js
+++ public/js/crearPropiedad.js
@@ -34,4 +34,20 @@ window.onload = function() {
         alert("Cargada correctamente");
         window.location.href = "/public/index.html";
     });
+    let cerrar = document.querySelectorAll(".close")[0];
+    let abrir = document.querySelectorAll(".cta")[0];
+    let modal = document.querySelectorAll(".modal")[0];
+    let modalC = document.querySelectorAll(".modal-container")[0];
+
+    abrir.addEventListener("click", function(e){
+        e.preventDefault();
+        modalC.style.opacity = "1";
+        modalC.style.visibility = "visible";
+        modal.classList.toggle("modal-close");
+    })
+    cerrar.addEventListener("click", function(){
+        modal.classList.toggle("modal-close");
+        modalC.style.opacity = "0"
+        modalC.style.visibility = "hidden"
+    })
 }
