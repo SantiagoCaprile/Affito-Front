80fbdaf04843935e8e256967d9265078d4f8fe5e
Santiago Caprile <santiagocaprile7@gmail.com>
2022-10-22 14:00:17 -0300

    Se agregó la autorizacion de ingreso validando respecto a los usuarios en la BD

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,19 @@
+async function usuarioValido(user, pass) {
+    const response = await fetch('http://localhost:5000/usuarios', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nombre: user,
+            password: pass
+        })
+    })
+    .then(response => response.json())
+    .then(data => data.valid);//valid va a ser true o false
+    return response;
+}
+
 window.onload = function() {
     const btn = document.getElementById('btn-login');
     const error = document.getElementById('error');
@@ -13,11 +29,12 @@ window.onload = function() {
 
     btn.addEventListener('click', async(e) => {
         e.preventDefault();
-        if( user.value === 'admin' && password.value == 'adminpass' ) {
-            window.location.href = './clientes.html';
+        const valid = await usuarioValido(user.value, password.value);
+        if (valid) {
+            sessionStorage.setItem('user', user.value);
+            window.location.href = './index.html';
         } else {
             error.classList.remove('oculto');
         }
     });
-
 }
